ba42c3dd2f94dd99fbb5a876f6522361e5251f27
webmarksdev <karenmarkswebdev@gmail.com>
2020-06-28 00:38:42 -0400

    fixed backend test data, added header comp to front end, added bootstrap & fontawesome

---
+++ backend/src/test-data/movies.js
@@ -0,0 +1,17 @@
+module.exports = movies = [
+    {
+        name: "movie1", 
+        description: "Movie1 Description",
+        imageLink: "/images/movie1.jpg",
+    },
+    {
+        name: "movie2", 
+        description: "Movie2 Description",
+        imageLink: "/images/movie2.jpg",
+    },
+    {
+        name: "movie3",
+        description: "Movie3 Description", 
+        imageLink: "/images/movie3.jpg",  
+    }
+]
---
+++ backend/src/test-data/reviews.js
@@ -0,0 +1,14 @@
+module.exports = reviews = [
+    {
+        feedback: "Movie1 Feedback",
+        rating: 5
+    },
+    {
+        feedback: "Movie2 Feedback",
+        rating: 4
+    },
+    {
+        feedback: "Movie3 Feedback",
+        rating: 3
+    }
+]
